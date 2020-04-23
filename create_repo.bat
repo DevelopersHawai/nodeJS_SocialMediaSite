52d83bd64794629e7bdeacc0beaee5d2238d802c
Corey Albright <coreymalbright@gmail.com>
2020-04-23 15:47:55 -0400

    remove setup repo script

--- create_repo.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# nodeJS_SocialMediaSite" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:Hawaiideveloper/nodeJS_SocialMediaSite.git
-git push -u origin master
