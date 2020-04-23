5885573e1bf3807c2986e9ef6bdba4b686384a10
Corey Albright <coreymalbright@gmail.com>
2020-04-23 15:11:18 -0400

    MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+const dotnev = require('dotenv');
+dotnev.config();
+
+
+
+//const MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://corey:<password>@nodeapi-70ovy.mongodb.net/test?retryWrites=true&w=majority";
+
+
+
+
+
+// Database Connection String goes here
+
+//const MongoClient = require('mongodb').MongoClient;
+
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        
+        //The line below uses Unified Topology & New URL Parser
+        { useUnifiedTopology: true, useNewUrlParser: true },
+        )
+.then(() => console.log('Database is connected')); 
+mongoose.connection.on("error", err => {
+   console.log('Connection Error with Database: ${err.message}');
+
+  //const uri = "mongodb+srv://corey:1234password@nodeapi-70ovy.mongodb.net/test?retryWrites=true&w=majority";
+  //const client = new MongoClient(uri, { useNewUrlParser: true });
+  //client.connect(err => {
+  //  const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+  //  client.close();
+  });
+
 
 
 // Import the routes 
@@ -16,7 +52,7 @@ app.use(morgan('dev'));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node JS API is listening on port: ${port}`);
 });
--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,8 @@
 
 
 exports.getPosts = (req, res) => {
-    res.send("Corey you are doing a great job and GOD is proud of you, keep going");
+    res.json({
+        posts: [{title: "Corey you are doing a great job and GOD is proud of you, keep going"}, { title: " Second thing is the secret to getting things done is to turn phone off and put headphones on and focus on one task at a time"}]
+    });
  
 };
