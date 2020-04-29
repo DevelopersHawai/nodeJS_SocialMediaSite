17fbf647b71f24265975be5fb54053a21e8076c3
Corey Albright <coreymalbright@gmail.com>
2020-04-29 01:16:32 -0400

    Validation and friendly error messages

--- validator/index.js
+++ validator/index.js
@@ -1,27 +1,25 @@
 //review video at 9:42
-
 exports.createPostValidator = (req, res, next) => {
-    //title
-    req.check('title', "write a title").notEmtpy();
-    req.check('title', "title must be between 4 to 200 characters").isLength({
+    // title
+    req.check('title', 'Write a title').notEmpty();
+    req.check('title', 'Title must be between 4 to 150 characters').isLength({
         min: 4,
-        max: 200
-    }); 
-        //body
-        req.check("body", "When GOD chooses your talent, there is no stopping you").notEmtpy();
-        req.check("title", "Title must be between 4 to 2000 characters").isLength({
-            min: 4,
-            max: 2000
-        }); 
-        //check for errors
-        const errors = req.validationErrors();
-         // error show the first one as rhey happen
-         
-         if(errors) {
-             const firstError = errors.map(error => error.msg)[0];
-             return res.status(400).json({error: firstError});
-         }
-         // Proceed to next middleware
-         next();
-         };
+        max: 150
+    });
+    // body
+    req.check('body', 'Write a body').notEmpty();
+    req.check('body', 'Body must be between 4 to 2000 characters').isLength({
+        min: 4,
+        max: 2000
+    });
+    // check for errors
+    const errors = req.validationErrors();
+          // if error show the first one as they happen
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+};
 
