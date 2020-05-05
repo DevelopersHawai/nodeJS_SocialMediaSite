9544048b38351495e9d516f11aaef2a350769ee8
Corey Albright <coreymalbright@gmail.com>
2020-05-05 01:45:48 -0400

    Virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
 
 
 // This where we set the scaffold to receive the profile creation
@@ -27,7 +28,34 @@ const userSchema = new mongoose.Schema({
 })
 
 /**
- * Virtual fields are not persisted to the database
+ * Virtual fields are not persisted to the database (password will be a virtual field with logic)
  */
+
+ //virtual field
+ userSchema.virtual('password')
+ .set(function(password) {
+     //create a temp passowrd 
+     this._password = password 
+     // Next we add some salt 
+     this.salt = uuidv1()
+     //encrypt the password()
+     this.hashed_password = this.encryptPassword(password)
+ })
+ .get(function() {
+     return this._password
+ })
+// methods to our schema [ our encryption process]
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if(!password) return "";
+        try {
+        return crypto.createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+        } catch (err) {
+                return ""
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
