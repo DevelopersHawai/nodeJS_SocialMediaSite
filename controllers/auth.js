417b675018996a744db4284cbfbf581fe66b7190
Corey Albright <coreymalbright@gmail.com>
2020-05-05 14:03:32 -0400

    How to run posts

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,5 +9,5 @@ exports.signup = async (req, res) => {
     })
     const user = await new User(req.body)
     await user.save()
-    res.status(200).json({user});
+    res.status(200).json({message: "Signup Successful! Please login"});
 }
