b3ffd026d63eb2312f897f8a3a3cafd2c65da4c2
Corey Albright <coreymalbright@gmail.com>
2020-05-05 21:01:42 -0400

    sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const User = require("../models/user");
 
 
@@ -10,4 +12,21 @@ exports.signup = async (req, res) => {
     const user = await new User(req.body)
     await user.save()
     res.status(200).json({message: "Signup Successful! Please login"});
+};
+
+exports.signin = (req, res) => {
+    //find the user based on email
+
+    // if error or no user 
+
+    // If user is located then we authenticate
+
+    // generate a token user id and secret
+
+    // persist the token as 't' in cookie with expiry date
+
+    //return response with user and token to frontend client
+
+
 }
+
