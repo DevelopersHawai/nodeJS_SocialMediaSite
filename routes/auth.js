b5ad31113d5ab96fbcbe79b6c7c42863dd7d2562
Corey Albright <coreymalbright@gmail.com>
2020-05-05 23:38:02 -0400

    signouts now possible

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 //Now adding User Signin
 //Based on jsonwebtoken
 const express = require("express");
-const { signup, signin } = require("../controllers/auth");
+const { signup, signin, signout } = require("../controllers/auth");
 //const {createPost} = require("../controllers/post");  //This is using the "object destructuring"
 const {userSignupValidator} = require("../validator"); // you dont need to put index
 
@@ -15,6 +15,7 @@ const router = express.Router();
 // The router here allows us to post information
 router.post("/signup", userSignupValidator, signup); //validator method
 router.post("/signin", signin); //signin
+router.get("/signout", signout); //signout
 //The above line looks at the app for middleware
 
 //Then we package the entire file as a function to be used as "router" later
