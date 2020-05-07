b24a9203a8a88252c332b3643e94eea1ea222d3e
Corey Albright <coreymalbright@gmail.com>
2020-05-07 03:48:12 -0400

    search for users feature

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { userById, allUsers, getUser } = require("../controllers/user");  //getUser imported by object destruct
-
+const {requireSignin } = require("../controllers/auth");
 //The router allows us to get receive information
 const router = express.Router();
 
 router.get("/users", allUsers);
-router.get("/user/:userId", getUser); // part of the controllers user to show single user
+router.get("/user/:userId", requireSignin, getUser); // part of the controllers user to show single user
 
 // The router here allows us to post information
 // router.post("/signup", userSignupValidator, signup); //validator method
