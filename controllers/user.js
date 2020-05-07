ab371e9766e7a4e0be9272b65defc63184aba54f
Corey Albright <coreymalbright@gmail.com>
2020-05-07 13:09:46 -0400

    updating a user complete

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const User = require("../models/user");
 
-
+//This is the method used to sort users by id
 exports.userById = (req, res, next , id) => {
     User.findById(id).exec((err, user) => {
 if(err || !user) {
@@ -14,8 +15,9 @@ next();
 };
 
 
+
 // Shout out to Ryan Dhungel for this awesome method
-//
+// This is the method to check authorization
 exports.hasAuthorization = (req, res, next) => {
     const authorized = req.profile && req.auth && req.profile._id === req.auth._id
     if(!authorized) {
@@ -25,8 +27,8 @@ exports.hasAuthorization = (req, res, next) => {
     }
 };
 
+// This is the method to spit out all the users in the system
 exports.allUsers = (req, res ) => {
-
     User.find((err, users) => {
         if(err) {
             return res.status(400).json({
@@ -39,9 +41,27 @@ exports.allUsers = (req, res ) => {
     //So that they can do business quickly
 };
 
+//This is the method to get a single user and show you its contents once you are authenticated
 exports.getUser = (req, res) => {     //This is what makes a single profile work single user view
-req.profile.hashed_password = undefined;  //Hides the password from all users
-req.profile.salt = undefined;  //Then taints the password to be unusable
+    req.profile.hashed_password = undefined;  //Hides the password from all users
+    req.profile.salt = undefined;  //Then taints the password to be unusable
     return res.json(req.profile);
+};
 
+//Allows user to update their own profile
+exports.updateUser = (req, res, next) => { 
+    let user = req.profile
+    user = _.extend(user, req.body)  //extend - mutate (change )the source object 
+    user.update = Date.now()
+    user.save((err) => {
+          if(err) {
+              return res.status(400).json({
+                  error: "You are not authorized to perform the previous action"
+              })
+          }
+          req.profile.hashed_password = undefined;
+          req.profile.salt = undefined;
+          res.json({user})
+
+    });
 };
