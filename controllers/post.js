62fbdf75a8f2dc04d68f37745f58462c2c06e3e4
Corey Albright <coreymalbright@gmail.com>
2020-05-07 20:28:45 -0400

    create post with image

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,8 @@
 //Requires the formatting of the application's input with standards
 //It uses the file within model called post which sets the formatting
-const Post = require('../models/post')
-
+const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
 //Prints all posts to (postman) screen
 exports.getPosts = (req, res) => {
     const posts = Post.find()
@@ -14,21 +15,46 @@ exports.getPosts = (req, res) => {
 };
 
 exports.createPost = (req, res) => {
-    const post = new Post(req.body); 
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (fields, files) => {
+if(err) {
+    return res.stus(400).json({
+        error: "Image could not be uploaded"
+    })
+}
+//I will review this one a few more times as this is where the magic happens
+    let post = new Post(fields)
+    post.postedBy = req.profile
+    if(files.photo) {
+        post.photo.data = fs.readFileSync(files.photo.path)
+        post.photo.contentType = files.photo.type
+    }
+    post.save((err, result) => {
+        if(err) {
+            return res.status(400).json({
+                error:err
+            })
+        }
+        res.json(result)
+    })
+    })
+
+   // const post = new Post(req.body);
     //console.log("Creating  post: ", req.body);
-   // post.save((err, result) => {
+    // post.save((err, result) => {
     //    if(err) {
-   //         return res.status(400).json({
-   //             error: err
-   //         })
-   //     }
-   //     res.status(200).json({
-   //         post: result
-   //     });
-   post.save().then(result => {
-       res.json({
-           post: result
-       });
-   });
+    //         return res.status(400).json({
+    //             error: err
+    //         })
+    //     }
+    //     res.status(200).json({
+    //         post: result
+    //     });
+   // post.save().then(result => {
+   //     res.json({
+     //       post: result
+    //    });
+  //  });
 
 };
